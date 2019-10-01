35cbe369b86ca69e2126186a07b438a159854bdb
Thiago Souza <ThiagoSouza1421@hotmail.com>
2019-10-01 04:16:49 -0300

    Inicializando aplicação..

---
+++ js/index.js
@@ -0,0 +1,9 @@
+//
+//  Index.html
+//  RainSimulator
+//
+//  Created by Thiago Souza on 01/10/19.
+//  Copyright © 2019 Thiago Souza. All rights reserved.
+//
+
+alert('Initializing application..');
