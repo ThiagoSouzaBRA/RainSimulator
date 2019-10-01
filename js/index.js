ce2c7f2ae53ed3c51cc0cf8f8dd954cb14e34cee
Thiago Souza <ThiagoSouza1421@hotmail.com>
2019-10-01 05:56:26 -0300

    Adicionando Animação

--- js/index.js
+++ js/index.js
@@ -8,11 +8,16 @@
 
 var Animation;
 var running = true;
+var gotas = [];
+var qtdgotas = 150;
 
 function Start(){
     Animation = createAnimation(800,800);
     document.body.appendChild(Animation[0]);
-    setInterval(Update, 100);
+    createGotas(qtdgotas);
+
+    setInterval(Update, 10);
+
 }
 
 function Stop(){
@@ -21,8 +26,8 @@ function Stop(){
 
 function Update(){ 
     running == true ? (
-        clearAnimation(),
-        showBackground()
+        showBackground(),
+        showGotas(qtdgotas)
         ):(false);
 }
 
@@ -38,12 +43,48 @@ function createAnimation(widthWindow, heightWindow){
     return [this.canvasWindow,this.context];
 }
 
-function clearAnimation(){
-    Animation[1].clearRect(0, 0, Animation[0].width, Animation[0].height); 
-}
 
 function showBackground(){
+    Animation[1].clearRect(0, 0, Animation[0].width, Animation[0].height); 
     Animation[1].fillStyle = 'rgb(0, 0, 0)';
     Animation[1].fillRect(0, 0, Animation[0].width, Animation[0].height);
 }
 
+function gotasModel(){
+    this.x = Math.floor(Math.random() * (Animation[0].width - 0)) + 0;
+    this.y = Math.floor(Math.random() * (-800 + 0)) + 0,
+    this.speed = 10;
+    this.width = 3;
+    this.height = 20;
+    this.scale = (Math.floor(Math.random() * (1 + 1)) + 1)/3;
+
+    this.fall = function(){
+        this.y += this.speed;
+
+        this.y > Animation[0].height ?(
+            this.y = Math.floor(Math.random() * (-800 + 0)) + 0,
+            this.x = Math.floor(Math.random() * (Animation[0].width - 0)) + 0) : false;
+    }
+
+    this.show = function(){
+        Animation[1].fillStyle = 'blue';
+        Animation[1].fillRect(this.x, this.y, this.width * this.scale, this.height * this.scale);
+    }
+}
+
+function createGotas(quantidade){
+    
+    for(i=0; i <= quantidade; i++)
+    {
+        gotas[i] = new gotasModel();
+    }
+    
+}
+
+function showGotas(quantidade){
+    for(i=0; i <= quantidade; i++)
+    {
+        gotas[i].fall();
+        gotas[i].show();
+    }
+}
